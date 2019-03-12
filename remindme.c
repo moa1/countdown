49cfd63b4ad9fcae747df8ccdd3e475381c47877
toni <toni@ordinateur>
2019-03-12 02:35:54 +0100

    add to `remindme` the feature to specify reminder date ranges.

--- remindme.c
+++ remindme.c
@@ -1,3 +1,4 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,43 +8,48 @@
 int verbose_parsing = 0;
 
 void usage(char *msg) {
-	fprintf(stderr, "Usage: [OPTIONS] DATESFILE\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "OPTIONS:\n");
-	fprintf(stderr, "  -u  output reminders in the order in DATESFILE (sorting by date is default)\n");
-	fprintf(stderr, "  -c  enable color output\n");
-	fprintf(stderr, "  -v  increase verbosity (verbosity level should be in-between -2 and 1)\n");
-	fprintf(stderr, "  -V  decrease verbosity\n");
-	fprintf(stderr, "  -p  verbose parsing of DATESFILE (for debugging DATES)\n");
-	fprintf(stderr, "  -d  enable debugging output\n");
-	fprintf(stderr, "  -h  print this help\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "DATESFILE must contain one or multiple 'DATE / MESSAGE' lines.\n");
-	fprintf(stderr, "It may also contain comments starting with '#' and extending to the end of line.\n");
-	fprintf(stderr, "If DATESFILE is specified with '-- -', reminders are read from standard input.\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "DATE can have one of the following formats:\n");
-	fprintf(stderr, "7:4 or 07:04 (today or tomorrow, seconds=0)\n");
-	fprintf(stderr, "7:4:59 (today or tomorrow)\n");
-	fprintf(stderr, "Monday 7:4 (weekday, in this or next week, seconds=0)\n");
-	fprintf(stderr, "Monday 7:4:59 (weekday, in this or next week)\n");
-	fprintf(stderr, "2014-07-22 7:4 (seconds=0)\n");
-	fprintf(stderr, "2014-07-22 7:4:59\n");
-	fprintf(stderr, "\n");
-	fprintf(stderr, "MESSAGE may be any string.\n");
-	fprintf(stderr, "\n");
+	if (!verbose_parsing) {
+		fprintf(stderr, "Usage: [OPTIONS] DATESFILE\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "OPTIONS:\n");
+		fprintf(stderr, "  -u  output reminders in the order in DATESFILE (sorting by date is default)\n");
+		fprintf(stderr, "  -c  enable color output\n");
+		fprintf(stderr, "  -v  increase verbosity (verbosity level should be in-between -2 and 1)\n");
+		fprintf(stderr, "  -V  decrease verbosity\n");
+		fprintf(stderr, "  -p  verbose parsing of DATESFILE (for debugging DATES)\n");
+		fprintf(stderr, "  -d  enable debugging output\n");
+		fprintf(stderr, "  -h  print this help\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "DATESFILE must contain one or multiple 'DATE / MESSAGE' lines.\n");
+		fprintf(stderr, "It may also contain comments starting with '#' and extending to the end of line.\n");
+		fprintf(stderr, "If DATESFILE is specified with '-- -', reminders are read from standard input.\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "DATE can have one of the following formats:\n");
+		fprintf(stderr, "7:4 or 07:04 (today or tomorrow, seconds=0)\n");
+		fprintf(stderr, "7:4:59 (today or tomorrow)\n");
+		fprintf(stderr, "Monday 7:4 (weekday, in this or next week, seconds=0)\n");
+		fprintf(stderr, "Monday 7:4:59 (weekday, in this or next week)\n");
+		fprintf(stderr, "2014-07-22 7:4 (seconds=0)\n");
+		fprintf(stderr, "2014-07-22 7:4:59\n");
+		fprintf(stderr, "DATE can also be a date range of two dates of above formats separated by '~'.\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "MESSAGE may be any string.\n");
+		fprintf(stderr, "\n");
+	}
 	if (msg != NULL) {
 		fprintf(stderr, "Error: %s\n", msg);
 	}
 }
 
 typedef struct reminder_struct {
-	struct tm *date;
+	struct tm *from;
+	struct tm *until;
 	char *message;
 } reminder;
 
 void print_reminder(const reminder *reminder) {
-	printf("date=%s", asctime(reminder->date));
+	printf("from=%s", asctime(reminder->from));
+	printf("until=%s", asctime(reminder->until));
 	printf("message=%s\n", reminder->message);
 }
 
@@ -61,14 +67,16 @@ void parse_datesfile(FILE *stream, int *parsed_reminders_num, reminder **parsed_
 	reminder *reminders = NULL;
 
 	// add `tm_date` to `reminders`.
-	void add_reminder(const struct tm *tm_date, const char *message) {
+	void add_reminder(const struct tm * const tm_date_from, const struct tm * const tm_date_until, const char *message) {
 		reminders = (reminder*)realloc(reminders, sizeof(reminder) * (reminders_num + 1));
 		if (reminders == NULL) {
 			perror("realloc error");
 			exit(3);
 		}
-		reminders[reminders_num].date = (struct tm*)safe_malloc(sizeof(struct tm));
-		memcpy(reminders[reminders_num].date, tm_date, sizeof(struct tm));
+		reminders[reminders_num].from = (struct tm*)safe_malloc(sizeof(struct tm));
+		memcpy(reminders[reminders_num].from, tm_date_from, sizeof(struct tm));
+		reminders[reminders_num].until = (struct tm*)safe_malloc(sizeof(struct tm));
+		memcpy(reminders[reminders_num].until, tm_date_until, sizeof(struct tm));
 		int length = 0;
 		for (length = 0;; length++) { if (message[length] == '\0') { length++; break;}}
 		reminders[reminders_num].message = (char*)safe_malloc(sizeof(char) * length);
@@ -95,7 +103,6 @@ void parse_datesfile(FILE *stream, int *parsed_reminders_num, reminder **parsed_
 		return ch == ' ' || ch == '\t' || ch == '\r';
 	}
 
-	struct tm tm_date;
 	struct tm tm_now;
 	get_tm_now(&tm_now);
 	void parse_date(char* field, struct tm *tm_date_ptr) {
@@ -113,6 +120,32 @@ void parse_datesfile(FILE *stream, int *parsed_reminders_num, reminder **parsed_
 		}
 	}
 
+	void parse_date_range(char* field, struct tm* from, struct tm* until) {
+		char* dash = strchr(field, '~');
+		if (dash) {
+			if (verbose_parsing) fprintf(stderr, "parsing RANGE '%s'\n", field);
+			char *ptr = dash;
+			for (ptr=dash;; ptr--) {if (!char_is_whitespace(ptr[-1])) break;}
+			ptr[0] = '\0';
+			parse_date(field, from);
+			char* field2 = dash;
+			for (field2 = dash+1;; field2++) {if (!char_is_whitespace(*field2)) break;}
+			parse_date(field2, until);
+			// check that `until` is after `from`
+			if (tm_diff(from, until) > 0) {
+				usage("FROM is later than UNTIL:");
+				fprintf(stderr, "%s = %s\n", field, field2);
+				exit(2);
+			}
+		} else {
+			parse_date(field, from);
+			memcpy(until, from, sizeof(struct tm));
+			until->tm_sec--; // later this means until=infinity
+		}
+	}
+
+	struct tm tm_date_from;
+	struct tm tm_date_until;
 	typedef enum {DATE, IGNORE, COMMENT, WHITESPACE, WHITE_TO_MESSAGE, MESSAGE} state_enum;
 	state_enum state = DATE; // ignore whitespace, wait for date
 	do {
@@ -149,7 +182,7 @@ void parse_datesfile(FILE *stream, int *parsed_reminders_num, reminder **parsed_
 				}
 				if (ch == '/') {
 					field[field_count++] = '\0';
-					parse_date(field, &tm_date);
+					parse_date_range(field, &tm_date_from, &tm_date_until);
 					field_count = 0;
 					state = WHITE_TO_MESSAGE; //skip to beginning of message
 				} else {
@@ -161,7 +194,7 @@ void parse_datesfile(FILE *stream, int *parsed_reminders_num, reminder **parsed_
 				state = MESSAGE;
 				if (ch == '\n') {
 					field[field_count++] = '\0';
-					add_reminder(&tm_date, field);
+					add_reminder(&tm_date_from, &tm_date_until, field);
 					field_count = 0;
 					state = IGNORE;
 				} else {
@@ -256,7 +289,7 @@ int main(int argc, const char **argv) {
 	int compare_reminders_tm(const void *r1p, const void *r2p) {
 		const reminder *r1 = (const reminder*) r1p;
 		const reminder *r2 = (const reminder*) r2p;
-		return compare_tm(r1->date, r2->date);
+		return compare_tm(r1->from, r2->from);
 	}
 	if (sorted)
 		qsort(reminders, reminders_num, sizeof(reminder), compare_reminders_tm);
@@ -274,9 +307,17 @@ int main(int argc, const char **argv) {
 	int first_today = 1;
 	int first_week = 1;
 	for (int i = 0; i < reminders_num; i++) {
-		double seconds = tm_diff_to_now_seconds(reminders[i].date);
+		double seconds = tm_diff_to_now_seconds(reminders[i].from);
+		double seconds_until;
+		if (tm_diff(reminders[i].until, reminders[i].from) > 0) {
+			seconds_until = tm_diff_to_now_seconds(reminders[i].until);
+		} else {
+			seconds_until = INFINITY;
+		}
+
 		if (debug) {
-			printf("seconds=%f ", seconds); print_reminder(&reminders[i]);
+			printf("seconds=%f seconds_until=%f\n", seconds, seconds_until);
+			print_reminder(&reminders[i]);
 		}
 
 		char *message = reminders[i].message;
@@ -292,7 +333,7 @@ int main(int argc, const char **argv) {
 		const int day_7 = day*7;
 
 		// do nothing
-		if (seconds < -day) continue;
+		if (seconds < -day || seconds_until < day_7) continue;
 
 		if (verbose > 0) {
 			if (seconds < 0) {
@@ -364,7 +405,7 @@ int main(int argc, const char **argv) {
 		} else if (seconds < day) {
 			printf(" %s%s%s\n", color_green, message, color_reset);
 		} else if (seconds < day_7) {
-			char *date = asctime(reminders[i].date);
+			char *date = asctime(reminders[i].from);
 			if (date[strlen(date)-1] == '\n') {
 				date[strlen(date)-1] = '\0';
 			}
